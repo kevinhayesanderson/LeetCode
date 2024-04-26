fccc0e5c4d2a89ed2791ccc449569c1c866df41b
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-04-26 23:46:44 +0530

    hashset using LL as buckets

--- test_1/LeetCode/HashSet/MyHashSet.cs
+++ test_1/LeetCode/HashSet/MyHashSet.cs
@@ -2,88 +2,71 @@
 
 namespace LeetCode.HashSet
 {
-    public class MyHashSet
+    public class Bucket_LL
     {
-        private Bucket[] buckets;
-        private int keyRange;
+        private readonly LinkedList<int> container;
 
-        public MyHashSet()
-        {
-            keyRange = 769;
-            buckets = new Bucket[keyRange];
-            for (int i = 0; i < keyRange; i++)
-            {
-                buckets[i] = new Bucket();
-            }
-        }
-
-        protected int hash(int key)
-        {
-            return key % keyRange;
-        }
-
-        public void Add(int key)
-        {
-            int bucketIndex = hash(key);
-            buckets[bucketIndex].insert(key);
-        }
-
-        public void Remove(int key)
-        {
-            int bucketIndex = hash(key);
-            buckets[bucketIndex].delete(key);
-        }
-
-        public bool Contains(int key)
-        {
-            int bucketIndex = hash(key);
-            return buckets[bucketIndex].exists(key);
-        }
-    }
-    public class Bucket
-    {
-        LinkedList<int> container;
-
-        public Bucket()
+        public Bucket_LL()
         {
             container = new LinkedList<int>();
         }
 
-        public void insert(int key)
-        {
-            int index = indexOf(container, key);
-            if (index != -1)
-            {
-                container.AddFirst(key);
-            }
-
-        }
-
-        private int indexOf(LinkedList<int> ll, int key)
-        {
-            int i = 0;
-            var head = ll.First;
-            while (head != null)
-            {
-                if (head.Value == key)
-                {
-                    return i;
-                }
-                head = head.Next;
-                i++;
-            }
-            return -1;
-        }
-
-        public void delete(int key)
+        public void Delete(int key)
         {
             container.Remove(key);
         }
 
-        public bool exists(int key)
+        public bool Exists(int key)
         {
             return container.Contains(key);
         }
 
+        public void Insert(int key)
+        {
+            var node = container.Find(key);
+            if (node == null)
+            {
+                container.AddFirst(key);
+            }
+        }
+    }
+
+    public class MyHashSet
+    {
+        private readonly Bucket_LL[] buckets;
+        private readonly int keyRange;
+
+        public MyHashSet()
+        {
+            keyRange = 769;
+            buckets = new Bucket_LL[keyRange];
+            for (int i = 0; i < keyRange; i++)
+            {
+                buckets[i] = new Bucket_LL();
+            }
+        }
+
+        public void Add(int key)
+        {
+            int bucketIndex = Hash(key);
+            buckets[bucketIndex].Insert(key);
+        }
+
+        public bool Contains(int key)
+        {
+            int bucketIndex = Hash(key);
+            return buckets[bucketIndex].Exists(key);
+        }
+
+        public void Remove(int key)
+        {
+            int bucketIndex = Hash(key);
+            buckets[bucketIndex].Delete(key);
+        }
+
+        protected int Hash(int key)
+        {
+            return key % keyRange;
+        }
     }
 }
